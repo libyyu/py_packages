99b92bd5601397ca2554af02ed4c2ce40f7d76cc
libyyu <389465209@qq.com>
2024-06-13 21:54:26 +0800

    chromedriver for linux

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+
+def get_path_packaged(relative_path):
+    import os, sys
+    try:
+        base_path = sys._MEIPASS # pyinstaller打包后的路径
+    except AttributeError:
+        base_path = os.path.abspath(".") # 当前工作目录的路径
+    return os.path.normpath(os.path.join(base_path, relative_path)) # 返回实际路径
